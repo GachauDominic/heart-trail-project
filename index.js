82faa548352179dcd78cb155ccad7e6a6808dd5d
GachauDominic <152270107+GachauDominic@users.noreply.github.com>
2025-01-24 15:47:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,23 @@ bodyElem.addEventListener("mousemove", (event)=>{
     spanElem.remove();
   }, 3000);
 });
+
+function createHeartTrail(xPos, yPos) {
+  const spanElem = document.createElement("span");
+  spanElem.style.left = xPos + "px";
+  spanElem.style.top = yPos + "px";
+  const size = Math.random() * 100;
+  spanElem.style.width = size + "px";
+  spanElem.style.height = size + "px";
+  bodyElem.appendChild(spanElem);
+  setTimeout(() => {
+    spanElem.remove();
+  }, 3000);
+}
+
+bodyElem.addEventListener("touchmove", (event) => {
+  const touch = event.touches[0];
+  const xPos = touch.clientX;
+  const yPos = touch.clientY;
+  createHeartTrail(xPos, yPos);
+});
